5bfc45817385d6342d4211bd24ce11ffe7cb0939
Diego Donaggio <diegodonaggio90@gmail.com>
2025-02-07 11:11:53 +0000

    Moved form logic to server action

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useActionState } from "react";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -9,25 +9,17 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
+import { FormState, send } from '@/lib/actions';
+
+const initialState: FormState = {
+  errors: {},
+  inputs: {},
+  success: false,
+}
+
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target
-    setFormData((prevState) => ({ ...prevState, [name]: value }))
-  }
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    // Here you would typically send the form data to your backend
-    console.log("Form submitted:", formData)
-    // Reset form after submission
-    setFormData({ name: "", email: "", message: "" })
-  }
+  const [state, formAction, isPending] = useActionState(send, initialState);
 
   return (
     <main className="min-h-screen bg-zinc-100 dark:bg-zinc-900 p-4 sm:p-8">
@@ -35,51 +27,81 @@ export default function Contact() {
 
       <div className="max-w-2xl mx-auto my-12">
         <h1 className="text-3xl font-bold mb-8 text-center dark:text-white">Contact Me</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <form action={formAction} className="space-y-6">
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
               Name
             </label>
             <Input
+              className={state.errors?.name ? 'border-red-500 mt-2' : 'mt-2'}
               type="text"
               id="name"
               name="name"
-              value={formData.name}
-              onChange={handleChange}
+              defaultValue={state.inputs?.name}
+              placeholder="John Doe"
               required
-              className="mt-1"
             />
+
+            {state.errors?.name && (
+              <p className="mt-2 text-red-500 text-sm">
+                {state.errors?.name}
+              </p>
+            )}
           </div>
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
               Email
             </label>
             <Input
+              className={state.errors?.email ? 'border-red-500 mt-2' : 'mt-2'}
               type="email"
               id="email"
               name="email"
-              value={formData.email}
-              onChange={handleChange}
+              defaultValue={state.inputs?.email}
+              placeholder="John.Doe@gmail.com"
               required
-              className="mt-1"
             />
+
+            {state.errors?.email && (
+              <p className="mt-2 text-red-500 text-sm">
+                {state.errors?.email}
+              </p>
+            )}
           </div>
           <div>
             <label htmlFor="message" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
               Message
             </label>
             <Textarea
+              className={state.errors?.message ? 'border-red-500 mt-2' : 'mt-2'}
               id="message"
               name="message"
-              rows={4}
-              value={formData.message}
-              onChange={handleChange}
+              rows={8}
+              defaultValue={state.inputs?.message}
+              placeholder="Hello, I would like to talk about ..."
+              minLength={10}
               required
-              className="mt-1"
             />
+
+            {state.errors?.message && (
+              <p className="mt-2 text-red-500 text-sm">
+                {state.errors?.message}
+              </p>
+            )}
           </div>
-          <Button type="submit" className="w-full">
-            Send Message
+
+          {state.success && (
+            <p className="text-green-500 text-sm">
+              Your message has been sent successfully!
+            </p>
+          )}
+
+          <Button
+            className="w-full"
+            disabled={isPending}
+            type="submit"
+          >
+            {isPending ? "Sending ..." : "Send Message"}
           </Button>
         </form>
       </div>
